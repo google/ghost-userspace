166d059f05ddd87900b5407d2e72bb5a3ad06d86
Jack Humphries <jhumphri@google.com>
2022-03-17 16:31:49 -0700

    cl/420306805: ghOSt: Fix open source build error
    See https://github.com/google/ghost-userspace/issues/3.
    
    When compiling externally on a machine running an open source ghOSt kernel, the definitions in bpf/user/agent.h cause the compilation to fail because the definitions for the same types also exist in the Linux headers installed on the machine (so the definitions in bpf/user/agent.h are *redefinitions*).
    
    Thus, to fix this, we add the GHOST_BPF macro to include/uapi/linux/bpf.h in the ghOSt kernel. We check for this macro in the userspace code. If the macro exists, we do not redefine the ghOSt kernel BPF types. Otherwise, we do.
    
    This CL will be submitted concurrently with kcl/454355.

--- bpf/user/agent.h
+++ bpf/user/agent.h
@@ -26,6 +26,11 @@
 extern "C" {
 #endif
 
+#ifndef GHOST_BPF
+// The definitions below are needed when the userspace code is compiled on a
+// machine that is *not* running the ghOSt kernel and therefore does not have
+// the ghOSt declarations below in the bpf.h UAPI header.
+
 // From include/uapi/linux/bpf.h for the ghost kernel.
 
 struct bpf_ghost_sched {};
@@ -41,6 +46,12 @@ enum {
 };
 
 // end include/uapi/linux/bpf.h
+
+#else
+
+#define BPF_GHOST_SCHED_MAX_ATTACH_TYPE __MAX_BPF_ATTACH_TYPE
+
+#endif
 
 // Generic BPF helpers
 
