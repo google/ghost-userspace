d45ae4c906db29b7b79107cbe6b0966917c6e13e
Josh Don <joshdon@google.com>
2022-12-20 17:36:47 -0800

    ghOSt: Fix SpinFor helper
    When using virtualization, clock_gettime can take quite a while (~6us).
    Doing 100 of these in a loop can fool the spinner into thinking we got
    preempted. To fix this, bail out of the loop if we've already
    accumulated a sufficient delta.
    
    PiperOrigin-RevId: 479470628

--- lib/base.cc
+++ lib/base.cc
@@ -27,6 +27,7 @@
 #include "absl/debugging/symbolize.h"
 #include "absl/status/status.h"
 #include "absl/strings/str_format.h"
+#include "absl/time/time.h"
 #include "kernel/ghost_uapi.h"
 #include "lib/logging.h"
 
@@ -318,6 +319,12 @@ void SpinFor(absl::Duration remaining) {
 
     for (int i = 0; i < 100; ++i) {
       delta = MonotonicNow() - start;
+
+      // If clock_gettime is slow, we don't want to mistake that for a
+      // preemption.
+      if (delta > absl::Microseconds(10)) {
+        break;
+      }
     }
 
     // Don't count preempted time; if we were off cpu, the large delta
