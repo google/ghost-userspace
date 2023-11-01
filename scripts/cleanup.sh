7ce1a024ea74b9d40b3d1d13f594bb117f129cb2
Alan Yang <alan123piano@gmail.com>
2023-11-01 17:34:16 -0400

    cleanup no longer needs root user

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -4,9 +4,4 @@
 # This script cleans up the ghOst enclaves
 # https://github.com/google/ghost-userspace/issues/35
 
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root (sudo bash)"
-  exit
-fi
-
-for i in /sys/fs/ghost/enclave_*/ctl; do echo destroy > $i; done
+sudo sh -c "for i in /sys/fs/ghost/enclave_*/ctl; do echo destroy > $i; done"
