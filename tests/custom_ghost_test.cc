04526b75038d996300830d2c5d8f8b90c6acfbdd
Alan Yang <alan123piano@gmail.com>
2023-11-01 16:25:08 -0400

    Add basic custom test

---
+++ tests/custom_ghost_test.cc
@@ -0,0 +1,27 @@
+#include <chrono>
+#include <cstdlib>
+#include <iostream>
+#include <memory>
+#include <thread>
+#include <vector>
+
+#include "lib/ghost.h"
+
+int main() {
+    std::vector<std::unique_ptr<ghost::GhostThread>> threads;
+    int ctr = 0;
+    for (int threadId = 0; threadId < 5; ++threadId) {
+        threads.push_back(std::make_unique<ghost::GhostThread>(
+            ghost::GhostThread::KernelScheduler::kCfs, [ctr, threadId]() {
+                for (int i = 0; i < 100; ++i) {
+                    std::this_thread::sleep_for(
+                        std::chrono::milliseconds(threadId + 5));
+                    ctr = ctr + 1;
+                }
+            }));
+    }
+    for (const auto &t : threads) {
+        t->Join();
+    }
+    std::cout << "Result: " << ctr << std::endl;
+}
