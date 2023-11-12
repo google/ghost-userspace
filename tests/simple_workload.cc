ffbb8a737b4019de4a777ca09ee4aeb4a90c7396
Alan Yang <alan123piano@gmail.com>
2023-11-12 18:47:08 -0500

    busy work

--- tests/simple_workload.cc
+++ tests/simple_workload.cc
@@ -2,6 +2,7 @@
 
 #include <chrono>
 #include <cstdlib>
+#include <functional>
 #include <iostream>
 #include <memory>
 #include <mutex>
@@ -11,30 +12,40 @@
 #include "lib/base.h"
 #include "lib/ghost.h"
 
+using std::chrono::duration;
+using clock = std::chrono::steady_clock;
+
+template <typename T>
+std::function<void()> make_work(T duration) {
+    return [] {
+        auto t1 = clock::now();
+        while (clock::now() < t1 + duration) {
+        }
+    };
+}
+
 int main() {
     int NUM_THREADS = 1000;
     std::mutex m;
     std::vector<std::unique_ptr<ghost::GhostThread>> threads;
-    auto start = std::chrono::steady_clock::now();
+    auto start = clock::now();
 
     for (int i = 0; i < NUM_THREADS; ++i) {
         if (rand() % 10 == 1) {
             threads.push_back(std::make_unique<ghost::GhostThread>(
-                ghost::GhostThread::KernelScheduler::kGhost, []() {
-                    std::this_thread::sleep_for(std::chrono::milliseconds(10));
-                }));
+                ghost::GhostThread::KernelScheduler::kGhost,
+                make_work(duration::milliseconds(10))));
         } else {
             threads.push_back(std::make_unique<ghost::GhostThread>(
-                ghost::GhostThread::KernelScheduler::kGhost, [] {
-                    std::this_thread::sleep_for(std::chrono::microseconds(5));
-                }));
+                ghost::GhostThread::KernelScheduler::kGhost,
+                make_work(duration::microseconds(5))));
         }
     }
 
     for (const auto& t : threads) t->Join();
 
-    auto end = std::chrono::steady_clock::now();
-    std::chrono::duration<double> diff = end - start;
+    auto end = clock::now();
+    duration<double> diff = end - start;
     std::cout << "Finished running threads in " << diff.count() << " seconds"
               << std::endl;
 }
