fcefcfd0820b117b49a18184ef62075ed2881993
Alan Yang <alan123piano@gmail.com>
2023-11-12 18:49:49 -0500

    fix

--- tests/simple_workload.cc
+++ tests/simple_workload.cc
@@ -33,11 +33,11 @@ int main() {
         if (rand() % 10 == 1) {
             threads.push_back(std::make_unique<ghost::GhostThread>(
                 ghost::GhostThread::KernelScheduler::kGhost,
-                make_work(std::chrono::duration::milliseconds(10))));
+                make_work(std::chrono::milliseconds(10))));
         } else {
             threads.push_back(std::make_unique<ghost::GhostThread>(
                 ghost::GhostThread::KernelScheduler::kGhost,
-                make_work(std::chrono::duration::microseconds(5))));
+                make_work(std::chrono::microseconds(5))));
         }
     }
 
