b750381a6e8d63edcd2a4289d108b5a915072885
Alan Yang <alan123piano@gmail.com>
2023-11-14 22:13:24 -0500

    orc

--- orchestrator/orc.cpp
+++ orchestrator/orc.cpp
@@ -3,52 +3,72 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-int main(int argc, char* argv[]) {
-    // Create child process (which will run the scheduler)
+#include <functional>
+#include <iostream>
+
+pid_t delegate_to_child(std::function<void()> work) {
     pid_t child_pid = fork();
     if (child_pid == -1) {
-        printf("failed to create child process\n");
-        return 1;
+        perror("fork error");
+        exit(1);
     }
 
     if (child_pid == 0) {
-        // we are the child
-
-        // Run FIFO scheduler
-        char* args[] = {"/usr/bin/sudo", "../bazel-bin/fifo_per_cpu_agent",
-                        "--ghost_cpus", "0-1", NULL};
-        printf("Starting scheduler\n");
-        int status = execv(args[0], args);
-
-        printf("status: %d\n");
-
-        perror("execv failed");
-        return 1;
+        work();
+        exit(0);
     } else {
-        // we are the parent
-        printf("Child pid: %d\n", child_pid);
-
-        sleep(5);
-
-        printf("Killing scheduler\n");
-
-        if (kill(child_pid, SIGINT) == -1) {
-            perror("failed to kill child");
-            return 1;
-        }
-
-        int status;
-        waitpid(child_pid, &status, 0);
-
-        if (WIFEXITED(status)) {
-            printf("child exited with status %d\n", status);
-        } else if (WIFSIGNALED(status)) {
-            printf("child terminated by signal %d\n", WTERMSIG(status));
-        } else {
-            printf("child ended in unknown way\n");
-        }
+        return child_pid;
     }
 
+    // unreachable
+    exit(1);
+}
+
+void terminate_child(pid_t child_pid) {
+    if (kill(child_pid, SIGKILL) == -1) {
+        perror("failed to kill child");
+        exit(1);
+    }
+
+    printf("killing child process (pid=%d) ...\n", child_pid);
+    int status;
+    waitpid(child_pid, &status, 0);
+
+    if (WIFEXITED(status)) {
+        printf("child (pid=%d) exited with status %d\n", child_pid, status);
+    } else if (WIFSIGNALED(status)) {
+        printf("child (pid=%d) terminated by signal %d\n", child_pid,
+               WTERMSIG(status));
+    } else {
+        printf("child (pid=%d) ended in unknown way\n", child_pid);
+    }
+}
+
+int main(int argc, char* argv[]) {
+    // Create child to run enclave cleanup (HACK)
+    delegate_to_child([] {
+        char* args[] = {"../scripts/cleanup.sh"};
+        execv(args[0], args);
+        perror("failed to call cleanup.sh");
+        exit(1);
+    });
+
+    // Create child process (which will run the scheduler)
+    pid_t child_pid = delegate_to_child([] {
+        // run FIFO scheduler
+        char* args[] = {"/usr/bin/sudo", "../bazel-bin/fifo_per_cpu_agent",
+                        "--ghost_cpus", "0-1", NULL};
+        execv(args[0], args);
+        perror("execv failed");
+        exit(1);
+    });
+
+    printf("Child pid: %d\n", child_pid);
+
+    sleep(5);
+
+    printf("Killing scheduler\n");
+    terminate_child(child_pid);
     /**
      * Pseudocode:
      *
