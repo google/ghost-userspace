2c6a366d4b05b9297213e9db4c719891b8b2a5fc
Alan Yang <alan123piano@gmail.com>
2023-11-28 19:45:52 -0500

    fix

--- scripts/synthetic.sh
+++ scripts/synthetic.sh
@@ -1,1 +1,1 @@
-sudo bazel-bin/experiments/scripts/synthetic ghost 0.01 30us 10000 20000 5000 agent_shinjuku 5s
+sudo bazel-bin/experiments/scripts/synthetic.par ghost 0.01 30us 10000 20000 5000 agent_shinjuku 5s
