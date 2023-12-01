34c70352bffa683d300cb56dc84d1f0f3245d661
Alan Yang <alan123piano@gmail.com>
2023-11-30 21:48:47 -0500

    print

--- scripts/simple_workload_experiment.py
+++ scripts/simple_workload_experiment.py
@@ -48,6 +48,7 @@ def run_experiment(
     # Parse CSV portion of output and return it
     lines = [line.decode("utf-8").strip() for line in proc.stdout.splitlines()]
     csvlines = lines[lines.index("<csv>") : lines.index("</csv>")]
+    print(csvlines)
     assert len(csvlines) == 2
     keys = [key for key in csvlines[0].split(", ")]
     values = [value for value in csvlines[1].split(", ")]
