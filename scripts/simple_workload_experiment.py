81a89bd217af62eac982afaf6f4684ab9cbe0be1
Alan Yang <alan123piano@gmail.com>
2023-12-01 23:45:24 -0500

    cache experiment stages

--- scripts/simple_workload_experiment.py
+++ scripts/simple_workload_experiment.py
@@ -4,8 +4,7 @@ import argparse
 import csv
 from decimal import Decimal
 import subprocess
-import time
-from typing import Any, List, Tuple
+from typing import List, Tuple
 
 
 parser = argparse.ArgumentParser()
@@ -62,63 +61,94 @@ def run_experiment(
     return list(zip(keys, values))
 
 
+def run_sched_exp(orca_port: int, sched_type: str) -> List[List[str]]:
+    "Run the experiment for one scheduler type."
+
+    rows: List[List[str]] = []
+
+    preemption_interval_us = 500 if sched_type == "cFCFS" else 0
+    set_scheduler(
+        orca_port=orca_port,
+        sched_type=sched_type,
+        preemption_interval_us=preemption_interval_us,
+    )
+
+    for throughput in range(20000, 200000 + 1, 20000):
+        for proportion_long_jobs in [
+            Decimal("0"),
+            Decimal("0.01"),
+            Decimal("0.1"),
+            Decimal("0.5"),
+        ]:
+            print(
+                f"Starting experiment for sched_type={sched_type} "
+                f"throughput={throughput} "
+                f"proportion_long_jobs={proportion_long_jobs}"
+            )
+            stats = run_experiment(
+                sched_type=sched_type,
+                throughput=throughput,
+                runtime=5,
+                num_workers=10,
+                proportion_long_jobs=proportion_long_jobs,
+            )
+            if len(rows) == 0:
+                rows.append(
+                    [
+                        "sched_type",
+                        "preemption_interval_us",
+                        "throughput",
+                        "proportion_long_jobs",
+                    ]
+                    + [t[0] for t in stats]
+                )
+            rows.append(
+                [
+                    sched_type,
+                    str(preemption_interval_us),
+                    str(throughput),
+                    str(proportion_long_jobs),
+                ]
+                + [t[1] for t in stats]
+            )
+
+    return rows
+
+
 def main() -> None:
     args = parser.parse_args()
     orca_port: int = args.orca_port
     out_file: str = args.out_file
 
-    csvrows: List[List[Any]] = []
+    csvrows: List[List[str]] = []
 
     for sched_type in ["dFCFS", "cFCFS", "cfs"]:
-        preemption_interval_us = 500 if sched_type == "cFCFS" else 0
-        set_scheduler(
-            orca_port=orca_port,
-            sched_type=sched_type,
-            preemption_interval_us=preemption_interval_us,
-        )
+        # Check for intermediate result
+        tmpfile = f"{sched_type}.{out_file}.tmp"
+        try:
+            with open(tmpfile) as file:
+                reader = csv.reader(file)
+                rows = list(reader)
+                print(f"Found previous result for {sched_type}")
+        except:
+            print(f"Starting experiment for {sched_type}...")
+            rows = run_sched_exp(orca_port=orca_port, sched_type=sched_type)
 
-        for throughput in range(20000, 200000 + 1, 20000):
-            for proportion_long_jobs in [
-                Decimal("0"),
-                Decimal("0.01"),
-                Decimal("0.1"),
-                Decimal("0.5"),
-            ]:
-                stats = run_experiment(
-                    sched_type=sched_type,
-                    throughput=throughput,
-                    runtime=5,
-                    num_workers=10,
-                    proportion_long_jobs=proportion_long_jobs,
-                )
-                if len(csvrows) == 0:
-                    csvrows.append(
-                        [
-                            "sched_type",
-                            "preemption_interval_us",
-                            "throughput",
-                            "proportion_long_jobs",
-                        ]
-                        + [t[0] for t in stats]
-                    )
-                print(
-                    f"Finished experiment for sched_type={sched_type} throughput={throughput} proportion_long_jobs={proportion_long_jobs}"
-                )
-                csvrows.append(
-                    [
-                        sched_type,
-                        preemption_interval_us,
-                        throughput,
-                        proportion_long_jobs,
-                    ]
-                    + [t[1] for t in stats]
-                )
+        # Add header if not exists
+        if len(csvrows) == 0:
+            csvrows += rows[0:1]
+
+        # Add non-header rows to csv
+        csvrows += rows[1:]
+
+        # Write intermediate result
+        with open(tmpfile, mode="w") as file:
+            writer = csv.writer(file)
+            writer.writerows(rows)
 
     with open(out_file, mode="w") as file:
         writer = csv.writer(file)
-
-        for row in csvrows:
-            writer.writerow(row)
+        writer.writerows(csvrows)
 
     print("Finished!")
 
